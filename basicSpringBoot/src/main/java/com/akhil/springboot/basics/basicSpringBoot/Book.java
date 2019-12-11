e4df9808ccb90d07da007cf7ff73878b9a21f172
Akhil423 <akhilvarma423@gmail.com>
2019-12-11 18:12:40 +0530

    basic spring boot project

---
+++ basicSpringBoot/src/main/java/com/akhil/springboot/basics/basicSpringBoot/Book.java
@@ -0,0 +1,38 @@
+package com.akhil.springboot.basics.basicSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+
+public class Book {
+	
+	private Long id;
+	private String name;
+	private String author;
+	
+	public Book(Long id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", author=" + author + "]";
+	}
+	
+	
+
+}
