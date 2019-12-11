6b86d2d096e168edbbff859da82b582b69fb4c3a
Akhil423 <akhilvarma423@gmail.com>
2019-12-11 18:46:53 +0530

    initialised jpa project

---
+++ jpa-project/src/test/java/com/akhil/springboot/basics/jpaproject/JpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akhil.springboot.basics.jpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
