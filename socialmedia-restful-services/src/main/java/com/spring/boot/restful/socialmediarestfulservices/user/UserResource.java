1a04e3615df2ffcd351ea8f15987d9eb12facb1a
Akhil423 <akhilvarma423@gmail.com>
2019-12-12 19:57:59 +0530

    implemented user rest service with http post method

--- socialmedia-restful-services/src/main/java/com/spring/boot/restful/socialmediarestfulservices/user/UserResource.java
+++ socialmedia-restful-services/src/main/java/com/spring/boot/restful/socialmediarestfulservices/user/UserResource.java
@@ -1,12 +1,18 @@
 package com.spring.boot.restful.socialmediarestfulservices.user;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ServletRequestDataBinderFactory;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -26,6 +32,18 @@ public class UserResource {
 	public Optional<User> getUserById(@PathVariable int id) {
 		
 		return userService.findone(id);
+	}
+	
+	//createUser
+	
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@RequestBody User user){
+		
+		User createdUser = userService.save(user);
+		
+		URI loc  = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdUser.getId()).toUri();
+	
+		return ResponseEntity.created(loc).build();
 	}
 
 }
