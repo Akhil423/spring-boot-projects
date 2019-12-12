8a7f9a51b5e6c3132df1c051636745b4f351b7b5
Akhil423 <akhilvarma423@gmail.com>
2019-12-12 16:44:58 +0530

    adding path variable to rest api url

--- socialmedia-restful-services/src/main/java/com/spring/boot/restful/socialmediarestfulservices/HelloWorldController.java
+++ socialmedia-restful-services/src/main/java/com/spring/boot/restful/socialmediarestfulservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.spring.boot.restful.socialmediarestfulservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
@@ -24,5 +25,13 @@ public class HelloWorldController {
 			
 			
 			return new HelloBean("Hello World");
+		}
+		
+
+		@GetMapping("/hello-world-bean/pathvariable/{name}")
+		public HelloBean printHelloPathVariable(@PathVariable String name ) {
+			
+			
+			return new HelloBean(String.format("Hello World %s", name));
 		}
 }
